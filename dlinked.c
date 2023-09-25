7d46512738469ddd465a074597477be603a84d30
Kanishka Anand <kanishkaadz@gmail.com>
2023-09-25 21:13:16 +0530

    Day 8

---
+++ dlinked.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<malloc.h>
+struct Node
+{
+    int val;
+    struct Node *prv;
+    struct Node *next;
+};
+typedef struct Node N;
+N *start=NULL, *ptr, *tmp;
+void CreateNode();
+void PrintNode();
+
+void CreateNode()
+{
+    ptr=(N*)malloc(sizeof(N));
+    printf("\nEnter Value:");
+    scanf("%d",&ptr->val);
+    ptr->next=NULL;
+    if(start==NULL)
+    {
+        start=ptr;
+        ptr->prv=NULL;
+        tmp=ptr;
+    }
+    else
+    {
+        tmp->next=ptr;
+        ptr->prv=tmp;
+        tmp=ptr;
+    }
+}
+
+void PrintNode()
+{
+    ptr=start;
+    while(ptr->next!=NULL)
+    {
+        printf("%d-> ",ptr->val);
+        ptr=ptr->next;
+    }
+    while(ptr!=NULL)
+    {
+        printf("%d <-",ptr->val);
+        ptr=ptr->prv;
+    }
+}
+
+void main()
+{
+    int ch;
+    do
+    {
+        printf("\n -: M E N U :-");
+        printf("\n1 --> Create Node");
+        printf("\n2 --> Print Node");
+        printf("\n0 --> Exit");
+        printf("\nEnter Choice:");
+        scanf("%d",&ch);
+        switch(ch)
+        {
+            case 1: CreateNode(); break;
+            case 2: PrintNode(); break;
+        }
+    } while (ch!=0);
+    
+}
