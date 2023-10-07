dc42d2a8dbb0bfb7d29baea0823896197cf989c1
kanishkadz <kanishkaadz@gmail.com>
2023-10-07 10:21:29 +0530

    Day 20 II

--- stackll.c
+++ stackll.c
@@ -56,9 +56,23 @@ void pop()
     }
     else
     {
-        printf("\n%d is popped",pt->val);
+        printf("\n%d is popped",ptr->val);
         top=top->next;
         free(ptr);
+    }
+}
+
+void peek()
+{
+    ptr=top;
+    if(top==NULL)
+    {
+        printf("\nStack Underflow");
+        return;
+    }
+    else
+    {
+        printf("\nTop Value is: %d",ptr->val);
     }
 }
 
@@ -70,6 +84,8 @@ void main()
         printf("\n-: M E N U :-");
         printf("\n1 --> Push");
         printf("\n2 --> Status");
+        printf("\n3 --> Pop");
+        printf("\n4 --> Peek");
         printf("\n0 --> Exit");
         printf("\nEnter your choice:");
         scanf("%d",&ch);
@@ -77,6 +93,8 @@ void main()
         {
             case 1:push(); break;
             case 2:status(); break;
+            case 3:pop(); break;
+            case 4:peek(); break;
         }
     } while (ch!=0);
 }
