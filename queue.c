f7768f0124b9c2f9f7d40013755c228b37e8edf3
kanishkadz <kanishkaadz@gmail.com>
2023-10-09 20:14:37 +0530

    Day 22 I

---
+++ queue.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#define size 5
+int Q[size];
+int front =-1;
+int rear =-1;
+void Insert();
+void status();
+
+void Insert()
+{
+    int n;
+    if(front>rear)
+    {
+        printf("\nQueue Underflow");
+        return;
+    }
+    else if(front==-1 && rear==-1)
+    {
+        front=0;
+        rear=0;
+    }
+    else
+    {
+        rear=rear-1;
+    }
+    printf("\nEnter value to insert:");
+    scanf("%d",&n);
+    Q[rear]=n;
+}
+
+void Status()
+{
+    int i;
+    if(front==-1 && rear==-1 || front>rear)
+    {
+        printf("\nQueue Underflow");
+        return;
+    }
+    printf("\nQueue is now:");
+    for(i=front;i<=rear;i++)
+    {
+        printf("\n%d",Q[i]);
+    }
+}
+
+void main()
+{
+    int ch;
+    do
+    {
+        printf("\n-: M E N U :-");
+        printf("\n1 --> Insert");
+        printf("\n2 --> Status");
+        printf("\nEnter Choice:");
+        scanf("%d",&ch);
+        switch(ch)
+        {
+            case 1: Insert(); break;
+            case 2: Status(); break;
+        }
+    } while (ch!=0);   
+}
