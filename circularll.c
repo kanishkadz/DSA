a5eddd991ac71cf4fa9e73584f22ac9c442cefe2
kanishkadz <kanishkaadz@gmail.com>
2023-10-11 20:28:58 +0530

    Day 24 II

--- circularll.c
+++ circularll.c
@@ -1,64 +1,65 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 struct Node
 {
     int val;
-    struct NOde *next;
+    struct Node *next;
 };
 typedef struct Node N;
-N *start=NULL, *ptr, *tmp;
+N *start = NULL, *ptr, *tmp;
 void CreateList();
 void PrintList();
 
-void CreateList()
-{
-    ptr=(N*)malloc(sizeof(N));
-    printf("\nEnter value:");
-    scanf("%d",&ptr->val);
-    if(start==NULL)
-    {
-        start=ptr;
-        tmp=ptr;
-        ptr->next=start;
-    }
-    else
-    {
-        tmp->next=ptr;
-        tmp=ptr;
-        ptr->next=start;
-    }
-}
-
-void PrintList()
-{
-    if(start==NULL)
-    {
-        printf("\nEmpty List");
-        return;
-    }
-    ptr=start;
-    while(ptr->next!=start)
-    {
-        printf("%d -> ",ptr->val);
-        ptr=ptr->next;
-    }
-    printf("%d ",ptr->val);
-}
-
-void main()
+void main() 
 {
     int ch;
-    do
+    do 
     {
         printf("\n-: M E N U:-");
         printf("\n1 --> Create List");
         printf("\n2 --> Print List");
         printf("\n0 --> Exit");
-        printf("\nEnter Your Choice:");
-        switch(ch)
+        printf("\nEnter Your Choice: ");
+        scanf("%d", &ch);
+        switch(ch) 
         {
             case 1: CreateList(); break;
             case 2: PrintList(); break;
         }
-    }while(ch!=0); 
+    }while(ch != 0);
+}
+
+void CreateList() 
+{
+    ptr = (N*)malloc(sizeof(N));
+    printf("\nEnter value: ");
+    scanf("%d", &ptr->val);
+    if (start == NULL) 
+    {
+        start = ptr;
+        tmp = ptr;
+        ptr->next = start;
+    } 
+    else 
+    {
+        tmp->next = ptr;
+        tmp = ptr;
+        ptr->next = start;
+    }
+}
+
+void PrintList() 
+{
+    if (start == NULL) 
+    {
+        printf("\nEmpty List");
+        return;
+    }
+    ptr = start;
+    do 
+    {
+        printf("%d -> ", ptr->val);
+        ptr = ptr->next;
+    } while (ptr != start);
+    printf("%d ", ptr->val);
 }
