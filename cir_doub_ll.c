f2c9803883ccdddf0ee64a1e723c1ff4a67b423f
kanishkadz <kanishkaadz@gmail.com>
2023-10-18 21:22:18 +0530

    Commit 2

--- cir_doub_ll.c
+++ cir_doub_ll.c
@@ -8,3 +8,48 @@ struct Node
 };
 typedef struct Node N;
 N *start=NULL, *ptr, *tmp;
+
+void CreateNode()
+{
+    ptr=(N*)malloc(sizeof(N));
+    printf("\nEnter value:");
+    scanf("%d",&ptr->val);
+    if(start==NULL)
+    {
+        start=ptr;
+        ptr->prv=ptr;
+        ptr->next=ptr;
+        tmp=ptr;
+    }
+    else
+    {
+        tmp->next=ptr;
+        ptr->prv=tmp;
+        ptr->next=start;
+        start->prv=ptr;
+        tmp=ptr;
+    }
+}
+
+void Print()
+{
+    if(start==NULL)
+    {
+        printf("\nEmpty List");
+        return;
+    }
+    else
+    {
+        while(ptr->next!=start)
+        {
+            printf("%d-> ",ptr->val);
+            ptr=ptr->next;
+        }
+        while(ptr!=start)
+        {
+            printf(" %d<-",ptr->val);
+            ptr=ptr->prv;
+        }
+    }
+
+}
